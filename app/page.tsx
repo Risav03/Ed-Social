7d4758f4297cfe2829f42e9fdd3441f367e69db5
Risav03 <needleweed03@gmail.com>
2024-11-05 21:01:37 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-start flex-col p-16 max-md:px-4 max-md:py-10">
+      
+    </div>
+  );
+}
