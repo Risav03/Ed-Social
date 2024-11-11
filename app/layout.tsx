ec062630fd2291c52da011186f8d1014bd731f8b
Risav03 <needleweed03@gmail.com>
2024-11-12 01:53:15 +0530

    og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,30 @@ import { Providers } from "@/lib/utils/Providers";
 import { Slide, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 export const metadata: Metadata = {
   title: "Ed-Soc",
-  description: "Educational Social Media",
-};
+  description: "Assignment for House of Ed-Tech",
+  openGraph: {
+    title: "Ed-Soc",
+  description: "Assignment for House of Ed-Tech",
+    url: 'https://ed-social.vercel.app/',
+    siteName: 'Ed-Soc',
+    images: [
+      {
+        url: 'https://ed-social.vercel.app/og.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://ed-social.vercel.app/og.png', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ]
+  }
+}
 
 export default function RootLayout({
   children,
