2417800730f54faf0f445a45be6f8c4b4e558cbd
Risav03 <needleweed03@gmail.com>
2024-11-12 01:32:40 +0530

    loader z index

--- components/UI/loader.tsx
+++ components/UI/loader.tsx
@@ -3,7 +3,7 @@ import { RiLoader5Fill } from 'react-icons/ri'
 
 export const Loader = () => {
   return (
-    <div className='w-screen h-screen flex items-center justify-center fixed top-0 left-0 z-[1000] backdrop-blur-xl'>
+    <div className='w-screen h-screen flex items-center justify-center fixed top-0 left-0 z-[100000] backdrop-blur-xl'>
         <RiLoader5Fill className='text-[3rem] text-white animate-spin' />
     </div>
   )
